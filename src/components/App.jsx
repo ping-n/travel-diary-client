646936cfd281b739df31cf438d5fed5c4c4bbd63
PING NGE <ping.nge@hotmail.com>
2020-07-08 22:49:05 +1000

    added basic nav to root

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import GetData from "./GetData";
 import Search from "./Search";
+import NavBar from "../shared/NavBar";
+import "../styles/App.css";
 
 function App() {
   return (
     <>
+      <NavBar />
       <GetData />
       <Search />
     </>
