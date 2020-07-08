15b30bcc12fddef1f8a7071d79ba8c499b6fdea5
PING NGE <ping.nge@hotmail.com>
2020-07-08 22:22:43 +1000

    axios installed and used axios to fetch api data

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import axios from "axios";
 const API_KEY = process.env.REACT_APP_ZOMATO_API_KEY;
 
 class GetData extends React.Component {
+  state = { data: {} };
   async componentDidMount() {
-    const response = await fetch(
+    const response = await axios.get(
       "https://developers.zomato.com/api/v2.1/location_details?entity_id=259&entity_type=city",
       {
         headers: {
@@ -11,8 +13,8 @@ class GetData extends React.Component {
         },
       }
     );
-    const data = await response.json();
-    console.log(data);
+    const data = response.data;
+    this.setState({ data: data });
   }
 
   render() {
@@ -21,4 +23,3 @@ class GetData extends React.Component {
 }
 
 export default GetData;
-
