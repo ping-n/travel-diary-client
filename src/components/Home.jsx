3b33b263f6ccbed7324ff737cf6b37a99b423755
PING NGE <ping.nge@hotmail.com>
2020-07-09 23:09:26 +1000

    created home login protectedroute and signup components

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./Search";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to our App</h1>
+      <Search />
+    </>
+  );
+};
+
+export default Home;
