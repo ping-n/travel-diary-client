3c62097621d8b8c087f9d8e48b8dfe07714d6e67
PING NGE <ping.nge@hotmail.com>
2020-07-09 21:22:35 +1000

    installed semantic ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Search from "./Search";
 import NavBar from "../shared/NavBar";
+import Card from "./Card"
 
 function App() {
   return (
     <>
       <NavBar />
       <Search />
+      <Card />
     </>
   );
 }
