d66f066beeb00e05a1d45ad88eb0a6f1f90ee193
PING NGE <ping.nge@hotmail.com>
2020-07-09 13:40:20 +1000

    added state to getData, need to render data

--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -3,18 +3,42 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_ZOMATO_API_KEY;
 
 class GetData extends React.Component {
-  state = { data: [] };
-  async componentDidMount() {
-    const response = await axios.get(
-      `https://developers.zomato.com/api/v2.1/location_details?entity_id=259&entity_type=city`,
-      {
-        headers: {
-          "user-key": API_KEY,
-        },
-      }
-    );
-    const data = response.data;
-    this.setState({ data: data });
+  state = {
+    data: this.props.data,
+    request: false,
+    entity_id: "",
+    entity_type: "",
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+    if (this.props.data.entity_id !== false) {
+      console.log(this.state)
+      this.setState({
+        entity_id: this.props.data.entity_id,
+        entity_type: this.props.data.entity_type,
+        request: true,
+      });
+    }
+  }
+
+  async componentDidUpdate() {
+    console.log("update")
+    if (this.state.request) {
+      console.log("axios request via GetData");
+      const { entity_id, entity_type } = this.state;
+      const response = await axios.get(
+        `https://developers.zomato.com/api/v2.1/location_details?entity_id=${entity_id}&entity_type=${entity_type}`,
+        {
+          headers: {
+            "user-key": API_KEY,
+          },
+        }
+      );
+      const data = response.data;
+      console.log(data);
+      this.setState({ data: data, request: false  });
+    }
   }
 
   render() {
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,9 +7,9 @@ class Search extends React.Component {
   state = {
     searchKey: "",
     request: false,
-    entity_type: "",
-    entity_id: "",
-    city_name: "",
+    entity_type: false,
+    entity_id: false,
+    city_name: false,
   };
 
   onInputChange = (event) => {
@@ -30,7 +30,11 @@ class Search extends React.Component {
     console.log("componentDidUpdate - ", "request status: " + request);
     if (searchKey && request) {
       this.setState({ request: false });
-      console.log("performing axios get with api: " + API_KEY);
+      let partial_api_key =
+        API_KEY.substring(0, 4) +
+        "..." +
+        API_KEY.substring(API_KEY.length, API_KEY.length - 4);
+      console.log("performing axios get with api: " + partial_api_key);
       const response = await axios.get(
         `https://developers.zomato.com/api/v2.1/locations?query=${searchKey}`,
         {
@@ -40,7 +44,7 @@ class Search extends React.Component {
         }
       );
       console.log("zomato axios response: ", response);
-      if (response.data.location_suggestions.length > 0) {
+      try {
         const {
           entity_type,
           entity_id,
@@ -51,23 +55,42 @@ class Search extends React.Component {
           entity_id: entity_id,
           entity_type: entity_type,
         });
-        console.log(this.state);}
+        console.log(this.state);
+      } catch (error) {
+        console.log("location not found");
+      }
+      //   if (response.data.location_suggestions.length > 0) {
+      //     const {
+      //       entity_type,
+      //       entity_id,
+      //       city_name,
+      //     } = response.data.location_suggestions[0];
+      //     this.setState({
+      //       city_name: city_name,
+      //       entity_id: entity_id,
+      //       entity_type: entity_type,
+      //     });
+      //     console.log(this.state);}
+      // }
     }
   }
 
   render() {
-    const { searchKey } = this.state;
-    return (
-      // <GetData data={this.state}/>
-      <form onSubmit={this.onFormSubmit}>
-        <input
-          type="text"
-          id="searchKey"
-          value={searchKey}
-          placeholder="Enter city for best restaurants"
-          onChange={this.onInputChange}
-        />
-      </form>
+    const { searchKey, ...data } = this.state;
+        return (
+      <>
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            id="searchKey"
+            value={searchKey}
+            placeholder="Enter city for best restaurants"
+            onChange={this.onInputChange}
+          />
+          <GetData data={data} />
+
+        </form>
+      </>
     );
   }
 }
